fb03fe4476d067cc697c39affce11463c741a42e
Swicano <Swicano@users.noreply.github.com>
2019-12-06 17:42:09 -0500

    works in simplest case
    works on test file (child parents in order)

--- Day6.py
+++ Day6.py
@@ -4,7 +4,9 @@
 #   no, that seems too easy. buld it as I read it.
 
 # the input is a lot of lines, so we will save it as a file and import it
-inputfile = 'c:\\users\gfirest\Box Sync\Other shit\Github\AdventCode\Day6input.txt'
+#inputfile = 'c:\\users\gfirest\Box Sync\Other shit\Github\AdventCode\Day6input.txt'     # real input
+inputfile = 'c:\\users\gfirest\Box Sync\Other shit\Github\AdventCode\Day6inputtest.txt' #42 is answer
+
 
 # general structure of building the tree: each line is a parent-child link
 #   assume the current tree is a set of trees (multiple roots), search each
@@ -17,28 +19,38 @@ inputfile = 'c:\\users\gfirest\Box Sync\Other shit\Github\AdventCode\Day6input.t
 #   and save the parent with a null pointer. as we update it, if the child already exists, just update the pointer
 #   this should be fine, since no child has multiple parents...
 
-#  lets do it this way, but lazily, implement our linked list as just a list
-#  where the first value is a list of positions, and the rest oare values at that position
-#  [ [ list of next's], datalist]
-#
-#    so a lazy list of 1 item would be  [[-1], 'first']
-#    lets add something that points to 'first'
-#                                       [[-1,0],'first','pnt_to_frst']
-# now lets add another item that doesn't point anywhere yet
-#                                       [[-1,0,-1],'first','pnt_to_frst','second']
-# and something that points at it
-#                                       [[-1,0,-1,2],'first','pnt_to_frst','second', 'pnt->scnd']
-# if we then update first to point at second, we would change the -1 to 2
-# we can follow this new list [[-1,0,-1,2],'first','pnt_to_frst','second', 'pnt->scnd'], which doesn't really have a head
-#  by starting with a location, lets say 1, for the location of 
+#  lets do it this way, but lazily, implement our linked list as just 2 lists
+#    one that holds the data, one that holds the location of next()
 
-lazylinked = [[None],None]
+
+lazylinkedData = []
+lazylinkedNext = []
 
 with open(inputfile, 'r') as f:
     for line in f.readlines():
         par,child = line.strip().split(')')
-        #   print ( f"{child} orbits {par} directly")
-    
+        link = -1
+        
+        if par not in lazylinkedData:
+            lazylinkedData.append(par)
+            lazylinkedNext.append(-1)
+            
+        link = lazylinkedData.index(par)
+  
+        if child not in lazylinkedData:
+            lazylinkedData.append(child)
+            lazylinkedNext.append(-1)
+        lazylinkedNext[lazylinkedData.index(child)] = link
+
+
+start = 'L'
+
+next = lazylinkedData.index(start)
+guess = ''
+
+while next > -0.5:
+    guess = guess + lazylinkedData[next]
+    next = lazylinkedNext[next]
 
 
 
